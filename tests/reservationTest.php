d3fc40a81de169bab1839d448ac17d80012eb377
Berchech Salma <82614136+Salma-dev-coder@users.noreply.github.com>
2022-07-12 12:45:21 +0200

    Update reservationTest.php

--- tests/reservationTest.php
+++ tests/reservationTest.php
@@ -1,68 +1,57 @@
 <?php
-use App\Entity\BusinessDepartment;
-use App\Entity\Contact;
-use App\Form\ContactForm;
 use App\Entity\Admin\Reservation;
 use App\Form\Admin\ReservationType;
-use App\Repository\HotelRepository;
-use Doctrine\ORM\AbstractQuery;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Mapping\ClassMetadata;
-use Doctrine\ORM\QueryBuilder;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bridge\Doctrine\Form\DoctrineOrmExtension;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Form\Test\TypeTestCase;
 
-class reservationTest extends KernelTestCase
+class reservationTest extends TypeTestCase
 {
-     public function testBuildForm()
+   
+    public function testSubmitValidData()
     {
-        $kernel = self::bootKernel();
+        $formData = [
+            'id' => '18',
+            'userid' => '10',
+            'hotelid' => '6',
+            'roomid' => '11',
+            'name' => 'salma',
+            'surname' => 'berchech',
+            'email' => 'salmaber24@gmail.com',
+            'phone' => NULL,
+            'days' => '106',
+            'total' => '4240',
+            'message' => 'goood',
+            'note' => NULL,
+            'status' => 'New',
+            'updated_at' => NULL,
+        ];
 
-        /**
-         * @var $container ContainerInterface
-         */
-        $container = $kernel->getContainer();
-        $data =  array('id' => '18',
-        'userid' => '10',
-        'hotelid' => '6',
-        'roomid' => '11',
-        'name' => 'hajjajihajer',
-        'surname' => 'hajjajihajer20',
-        'email' => 'hajjajihajer24@gmail.com',
-        'phone' => NULL,
-        'checkin' => '2021-04-21 00:00:00',
-        'checkout' => '2021-08-05 00:00:00',
-        'days' => '106',
-        'total' => '4240',
-        'ip' => '::1',
-        'message' => 'goood',
-        'note' => NULL,
-        'status' => 'New',
-        'created_at' => '2021-03-12 11:29:09',
-        'updated_at' => NULL)
-        ;
+        $model = new Reservation();
+        // $model will retrieve data from the form submission; pass it as the second argument
+        $form = $this->factory->create(ReservationType::class, $model);
 
-            $resa = new Reservation();
-            $formFactory = $container->get("form.factory");
-            $form = $formFactory->create(ReservationType::class, $resa);
-
-        $contactToCompare = new Reservation();
-       
-
-        $expected = new Reservation();
-        // ...populate $object properties with the data stored in $formData
+        $expected = (new Reservation())
+            ->setUserid($formData['userid'])
+            ->setHotelid($formData['hotelid'])
+            ->setRoomId($formData['roomid'])
+            ->setName($formData['name'])
+            ->setSurname($formData['surname'])
+            ->setEmail($formData['email'])
+            ->setPhone($formData['phone'])
+            ->setDays($formData['days'])
+            ->setTotal($formData['total'])
+            ->setMessage($formData['message'])
+            ->setNote($formData['note'])
+            ->setStatus($formData['status']);
+        // ...populate $expected properties with the data stored in $formData
 
         // submit the data to the form directly
-        $form->submit($data);
+        $form->submit($formData);
 
         // This check ensures there are no transformation failures
         $this->assertTrue($form->isSynchronized());
 
         // check that $model was modified as expected when the form was submitted
-        $this->assertEquals($expected, $resa);
+        $this->assertEquals($expected, $model);
     }
 
-    
 }
